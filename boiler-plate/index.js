ae9f52c74f5e74757bfc019ed19cef00199f7f6c
seongjin <sokc625@gmail.com>
2021-04-13 00:25:09 +0900

    auth, logout

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 // config 폴더에 key.js에서 개발모드와 운영모드에 따라 mongoDB 연결을 다르게
@@ -30,7 +31,7 @@ app.get('/', (req, res) => {
 
 // postman 으로 테스트
 // Register Route 생성 코드
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
 
@@ -44,7 +45,7 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if(!user) {
@@ -60,7 +61,7 @@ app.post('/login', (req, res) => {
       if(!isMatch)
       return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
 
-      //비밀번호 까지 맏다면 토큰을 생성하기.
+      //비밀번호 까지 맞다면 토큰을 생성하기.
       //generateToken 는 User.js 에서 만든 메서드
       user.generateToken((err, user) => {
         if(err) return res.status(400).send(err);
@@ -75,6 +76,30 @@ app.post('/login', (req, res) => {
   })
 
 })
+
+// role 1 어드민    role 2 특정 부서 어드민
+// role 0 -> 일반유저   role 0이 아니면 관리자
+app.get('/api/users/auth', auth, (req, res) => {
+  //여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if(err) return res.json({ success: false, err});
+    return res.status(200).send({ success: true });
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
