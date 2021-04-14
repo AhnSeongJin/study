33e4b5c62407b13f73b0a988bbdc554b0a53c387
seongjin <sokc625@gmail.com>
2021-04-14 10:35:49 +0900

    Redux Setting

---
+++ boiler-plate/client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+// import user from './user_reducer';
+
+// 여러가지 reducer 를 combineReducers 를 이용해서 rootReducer 에 합친다.
+const rootReducer = combineReducers({
+  // user,
+})
+
+export default rootReducer;
--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css'; //CSS Framework
+// Redux middleware 사용 3가지
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+// 원래는 createStore만으로 store 생성 가능하지만, 
+// promise와 functions 을 받기위해 미들웨어 사용
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(Reducer,
+          //크롬에서 다운받은 redux extension 사용하기
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
